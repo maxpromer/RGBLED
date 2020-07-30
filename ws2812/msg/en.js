312550233da55781ba03031430646b43f6f5b68e
Max <max30012540@hotmail.com>
2020-07-31 00:02:25 +0700

    Rename

--- ws2812/msg/en.js
+++ ws2812/msg/en.js
@@ -1,29 +1,29 @@
-Blockly.Msg.NEOPIXEL_INIT_MESSAGE = "RGBLED initialize pin %1 length %2 mode %3";
+Blockly.Msg.NEOPIXEL_INIT_MESSAGE = "iRGBLED initialize pin %1 length %2 mode %3";
 Blockly.Msg.NEOPIXEL_INIT_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_SET_BRIGHTNESS_MESSAGE = "RGBLED pin %1 set brightness %2 %";
+Blockly.Msg.NEOPIXEL_SET_BRIGHTNESS_MESSAGE = "iRGBLED pin %1 set brightness %2 %";
 Blockly.Msg.NEOPIXEL_SET_BRIGHTNESS_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_SET_PIXEL_MESSAGE = "RGBLED pin %1 set pixel %2 color %3";
+Blockly.Msg.NEOPIXEL_SET_PIXEL_MESSAGE = "iRGBLED pin %1 set pixel %2 color %3";
 Blockly.Msg.NEOPIXEL_SET_PIXEL_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_SET_PIXEL_RGB_MESSAGE = "RGBLED pin %1 set pixel %2 color rgb( %3 , %4 , %5 )";
+Blockly.Msg.NEOPIXEL_SET_PIXEL_RGB_MESSAGE = "iRGBLED pin %1 set pixel %2 color rgb( %3 , %4 , %5 )";
 Blockly.Msg.NEOPIXEL_SET_PIXEL_RGB_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_FILL_MESSAGE = "RGBLED pin %1 fill all LED color %2";
+Blockly.Msg.NEOPIXEL_FILL_MESSAGE = "iRGBLED pin %1 fill all LED color %2";
 Blockly.Msg.NEOPIXEL_FILL_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_FILL_RGB_MESSAGE = "RGBLED pin %1 fill all LED color rgb( %2 , %3 , %4 )";
+Blockly.Msg.NEOPIXEL_FILL_RGB_MESSAGE = "iRGBLED pin %1 fill all LED color rgb( %2 , %3 , %4 )";
 Blockly.Msg.NEOPIXEL_FILL_RGB_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_SHOW_MESSAGE = "RGBLED pin %1 show";
+Blockly.Msg.NEOPIXEL_SHOW_MESSAGE = "iRGBLED pin %1 show";
 Blockly.Msg.NEOPIXEL_SHOW_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_CLEAR_MESSAGE = "RGBLED pin %1 clear";
+Blockly.Msg.NEOPIXEL_CLEAR_MESSAGE = "iRGBLED pin %1 clear";
 Blockly.Msg.NEOPIXEL_CLEAR_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_RAINBOW_MESSAGE = "RGBLED pin %1 rainbow Time(ms) %2";
+Blockly.Msg.NEOPIXEL_RAINBOW_MESSAGE = "iRGBLED pin %1 rainbow Time(ms) %2";
 Blockly.Msg.NEOPIXEL_RAINBOW_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_RAINBOW_CYCLE_MESSAGE = "RGBLED pin %1 rainbow cycle Time(ms) %2";
+Blockly.Msg.NEOPIXEL_RAINBOW_CYCLE_MESSAGE = "iRGBLED pin %1 rainbow cycle Time(ms) %2";
 Blockly.Msg.NEOPIXEL_RAINBOW_CYCLE_TOOLTIP = "";
--- ws2812/msg/th.js
+++ ws2812/msg/th.js
@@ -1,29 +1,29 @@
-Blockly.Msg.NEOPIXEL_INIT_MESSAGE = "RGBLED เริ่มต้นใช้งานที่ขา %1 ความยาว %2 ดวง โหมดสี %3";
+Blockly.Msg.NEOPIXEL_INIT_MESSAGE = "iRGBLED เริ่มต้นใช้งานที่ขา %1 ความยาว %2 ดวง โหมดสี %3";
 Blockly.Msg.NEOPIXEL_INIT_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_SET_BRIGHTNESS_MESSAGE = "RGBLED ขา %1 กำหนดความสว่าง %2 %";
+Blockly.Msg.NEOPIXEL_SET_BRIGHTNESS_MESSAGE = "iRGBLED ขา %1 กำหนดความสว่าง %2 %";
 Blockly.Msg.NEOPIXEL_SET_BRIGHTNESS_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_SET_PIXEL_MESSAGE = "RGBLED ขา %1 กำหนดดวงที่ %2 เป็นสี %3";
+Blockly.Msg.NEOPIXEL_SET_PIXEL_MESSAGE = "iRGBLED ขา %1 กำหนดดวงที่ %2 เป็นสี %3";
 Blockly.Msg.NEOPIXEL_SET_PIXEL_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_SET_PIXEL_RGB_MESSAGE = "RGBLED ขา %1 กำหนดดวงที่ %2 เป็นสี rgb( %3 , %4 , %5 )";
+Blockly.Msg.NEOPIXEL_SET_PIXEL_RGB_MESSAGE = "iRGBLED ขา %1 กำหนดดวงที่ %2 เป็นสี rgb( %3 , %4 , %5 )";
 Blockly.Msg.NEOPIXEL_SET_PIXEL_RGB_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_FILL_MESSAGE = "RGBLED ขา %1 กำหนดสีทุกดวงเป็น %2";
+Blockly.Msg.NEOPIXEL_FILL_MESSAGE = "iRGBLED ขา %1 กำหนดสีทุกดวงเป็น %2";
 Blockly.Msg.NEOPIXEL_FILL_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_FILL_RGB_MESSAGE = "RGBLED ขา %1 กำหนดสีทุกดวงเป็น rgb( %2 , %3 , %4 )";
+Blockly.Msg.NEOPIXEL_FILL_RGB_MESSAGE = "iRGBLED ขา %1 กำหนดสีทุกดวงเป็น rgb( %2 , %3 , %4 )";
 Blockly.Msg.NEOPIXEL_FILL_RGB_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_SHOW_MESSAGE = "RGBLED ขา %1 แสดงผล";
+Blockly.Msg.NEOPIXEL_SHOW_MESSAGE = "iRGBLED ขา %1 แสดงผล";
 Blockly.Msg.NEOPIXEL_SHOW_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_CLEAR_MESSAGE = "RGBLED ขา %1 ล้างค่า";
+Blockly.Msg.NEOPIXEL_CLEAR_MESSAGE = "iRGBLED ขา %1 ล้างค่า";
 Blockly.Msg.NEOPIXEL_CLEAR_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_RAINBOW_MESSAGE = "RGBLED ขา %1 สว่างไล่สี หน่วงเวลา (ms) %2";
+Blockly.Msg.NEOPIXEL_RAINBOW_MESSAGE = "iRGBLED ขา %1 สว่างไล่สี หน่วงเวลา (ms) %2";
 Blockly.Msg.NEOPIXEL_RAINBOW_TOOLTIP = "";
 
-Blockly.Msg.NEOPIXEL_RAINBOW_CYCLE_MESSAGE = "RGBLED ขา %1 สว่างไล่สีแบบวงรอบ หน่วงเวลา (ms) %2";
+Blockly.Msg.NEOPIXEL_RAINBOW_CYCLE_MESSAGE = "iRGBLED ขา %1 สว่างไล่สีแบบวงรอบ หน่วงเวลา (ms) %2";
 Blockly.Msg.NEOPIXEL_RAINBOW_CYCLE_TOOLTIP = "";
